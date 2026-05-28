edfaf9945bae9ea2fcf0af5af9885a266538e7ed
Robêrt <132081786+TrevorGlove@users.noreply.github.com>
2026-05-28 15:00:49 -0500

    Update PIDmotorposition.py

--- Motor Control/PIDmotorposition.py
+++ Motor Control/PIDmotorposition.py
@@ -1,30 +1,30 @@
 """
 @TrevorGlove https://github.com/TrevorGlove/motor_control
-Elaborado with Raspberry Pi Pico
+Developed with Raspberry Pi Pico
 """
 
 from machine import Pin, ADC, Timer
 from L298N import L298N
 
-# -----Declaración de pines----
+# -----Pin Declarations----
 
-# L298N y encoder
+# L298N and encoder
 Pin_L298N = {'IN2': 6, 'IN1': 7, 'ENA': 8}
 Pin_Encoder = {'A': 4, 'B': 5}
 
-# Potenciómetro
+# Potentiometer
 PinPOT = 26
 
-# -----Declaración de variables----
+# -----Variable Declarations----
 
-contador = 0   # Pulsos de encoder
-pv = 486       # Total de pulsos por vuelta
+counter = 0    # Encoder pulses
+ppr = 486      # Total pulses per revolution
 
 vA1 = 0        # vA(k-1)
 vB1 = 0        # vB(k-1)
 
-dirmotor = False  # F dirección hacia atras y V dirección hacia adelante
-gr = 0         # Grados de giro del motor
+motor_dir = False  # F for backward direction and T for forward direction
+deg = 0         # Motor rotation degrees
 
 # PID
 sp = 0
@@ -35,33 +35,33 @@ Ts = 0.1
 
 q = [K[0] + K[2]/Ts, -K[0] + K[1]*Ts - 2*K[2]/Ts, K[2]/Ts]
 
-# Valores auxilizares por transformacion PID de A/D
+# Auxiliary values for PID transformation from A/D
 # q =  [1.458, 0.5521, -0.9039]
 
-# -----Definición de funciones de interrupción----
+# -----Interrupt Function Definitions----
 
-def interrupcion(pin):
-    global contador, vA, vA1, vA2, vB, dirmotor
-    vA = A.value()  # Valor de Pin A de encoder
+def interrupt(pin):
+    global counter, vA, vA1, vA2, vB, motor_dir
+    vA = A.value()  # Encoder Pin A value
     if not vA1 and vA:
-        vB = B.value()  # Valor de Pin B de encoder
-        if vB == (False and dirmotor):
-            dirmotor = False
-        elif vB == (True and (not dirmotor)):
-            dirmotor = True
+        vB = B.value()  # Encoder Pin B value
+        if vB == (False and motor_dir):
+            motor_dir = False
+        elif vB == (True and (not motor_dir)):
+            motor_dir = True
     vA1 = vA
 
-    contador += 1 if not dirmotor else -1
+    counter += 1 if not motor_dir else -1
 
 def PID(timer):
-    global gr, contador, pot, sp, c, e, pv, Ts, q
+    global deg, counter, pot, sp, c, e, ppr, Ts, q
 
-    gr = contador*360/pv
+    deg = counter*360/ppr
 
     sp = pot*360/65535 - 180
-    e[0] = sp - gr
+    e[0] = sp - deg
 
-    # -----Control PID----
+    # -----PID Control----
 
     c[0] = c[1] + q[0]*e[0] + q[1]*e[1] + q[2]*e[2]
 
@@ -70,22 +70,22 @@ def PID(timer):
     e[1] = e[0]
     e[2] = e[1]
     
-    print(("Sp", int(sp), "Gr", int(gr)))
+    print(("Sp", int(sp), "Deg", int(deg)))
 
-# -----Configuración de pines----
+# -----Pin Configuration----
 
-# Lectura dfel ADC
+# ADC Reading
 adc = ADC(PinPOT)
 
-# Interrucpión en Pin A
+# Interrupt on Pin A
 A = Pin(Pin_Encoder['A'], Pin.IN)
 B = Pin(Pin_Encoder['B'], Pin.IN)
-A.irq(trigger=Pin.IRQ_RISING, handler=interrupcion)
+A.irq(trigger=Pin.IRQ_RISING, handler=interrupt)
 
-# Control de motor
+# Motor Control
 motor1 = L298N(Pin_L298N['IN1'], Pin_L298N['IN2'], Pin_L298N['ENA'])
 
-# Interrucpión por Timer
+# Timer Interrupt
 tim = Timer()
 tim.init(period=int(Ts * 1000), mode=Timer.PERIODIC, callback=PID)
 
